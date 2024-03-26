85b2d93e1f8954bc2c9f657505fc3e6cd7f19b99
Carlos Serrano <carpasse@gmail.com>
2024-03-26 13:10:29 +0100

    refactor: add alpha,beta,gamma styles

--- src/components/MArticlePreviewHeadline/MArticlePreviewHeadline.stories.ts
+++ src/components/MArticlePreviewHeadline/MArticlePreviewHeadline.stories.ts
@@ -14,3 +14,15 @@ type Story = StoryObj<{}>;
 export const Primary: Story = createWebComponentStory(
   `<h2 is="m-article-preview-headline" link="https://www.dailymailint.co.uk/news/article-13088871/Prince-Harry-hits-criticism-Sussex-com-Royal-rebrand-defiantly-insisting-Meghan-not-broken.html">Prince Harry and Meghan hit back after criticism of Sussex.com launch and family rebrand, defiantly insisting they 'will not be broken'</h2>`
 );
+
+export const WithSignpost: Story = createWebComponentStory(
+  `<h2 is="m-article-preview-headline" link="https://www.dailymailint.co.uk/news/article-13088871/Prince-Harry-hits-criticism-Sussex-com-Royal-rebrand-defiantly-insisting-Meghan-not-broken.html">
+    <div is="m-badges">
+      <span is="m-plus-badge">M+</span>
+      <span is="m-badge">BREAKING NEWS</span>
+      <span is="m-badge">EXCLUSIVE</span>
+      <span is="m-live-badge">LIVE</span>
+    </div>
+    Prince Harry and Meghan hit back after criticism of Sussex.com launch and family rebrand, defiantly insisting they 'will not be broken'
+  </h2>`
+);
